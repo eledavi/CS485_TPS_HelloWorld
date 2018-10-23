d104ed9c4acc5a76bcdc852689f4ed3b7030b0d8
eledavi <eledavi7@gmail.com>
2018-10-23 11:17:19 -0600

    Initial Commit

---
+++ XClicker-TPS-HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XClicker-TPS-HelloWorld
+//
+//  Created by Elena on 10/23/18.
+//  Copyright © 2018 Elena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ XClicker-TPS-HelloWorldTests/XClicker_TPS_HelloWorldTests.swift
@@ -0,0 +1,34 @@
+//
+//  XClicker_TPS_HelloWorldTests.swift
+//  XClicker-TPS-HelloWorldTests
+//
+//  Created by Elena on 10/23/18.
+//  Copyright © 2018 Elena. All rights reserved.
+//
+
+import XCTest
+@testable import XClicker_TPS_HelloWorld
+
+class XClicker_TPS_HelloWorldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
